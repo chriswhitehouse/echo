83f12b5d35debb2ad81dd7852aef994f01d6588f
Chris Whitehouse <chris_whitehouse@hotmail.com>
2021-02-03 17:52:20 +0000

    full functionality

--- lib/echo.rb
+++ lib/echo.rb
@@ -1,7 +1,27 @@
+def run
+  string = ""
+
+  while true
+    puts "Say something:"
+    string = gets.chomp
+    if string == "exit"
+      break
+    else
+      echo(string)
+    end
+  end
+
+
+
+end
+
+
 def echo(string)
   t = Time.now
   date = t.strftime("%F")
   time = t.strftime("%H:%M")
 
-  "#{date} | #{time} | You said: '#{string}'!"
+  puts "#{date} | #{time} | You said: '#{string}'!"
 end
+
+run
